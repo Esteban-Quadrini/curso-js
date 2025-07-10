10fa331eafab8a31cff9c341bf68b69b820e51bc
Esteban-Quadrini <estebanqu02@gmail.com>
2025-07-10 13:37:33 -0300

    primeras practicas

---
+++ app..js
@@ -0,0 +1,25 @@
+//primera clase// 
+
+let nombreUsuario = "Esteban"
+let edad = 33
+let casado = false 
+
+const DNI = "36073595"
+
+nombreUsuario = "Esteban Quadrini"
+
+console.log(casado)
+console.log(edad + " años")
+console.log(nombreUsuario)
+console.log(DNI)
+
+
+alert("Bienvenido " + nombreUsuario + ", tenes " + edad + " años")
+
+let continuar = confirm("quieres continuar?")
+console.log(continuar)
+
+let edadNueva = prompt("Decime tu edad")
+console.log(edadNueva)
+
+
